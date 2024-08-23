906adb1739721eefd9783156d9dca9d905a8a0b1
auro <auro@DESKTOP-AV8I9BT>
2024-08-23 21:22:54 +0300

    Add project files.

---
+++ ArrayPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] tamSayi = new int[10];
+
+        for (int i = 0; i < tamSayi.Length; i++)  // Bu diziyi for döngüsü ile dolduruyoruz.
+        {
+            tamSayi[i] = (i +1) * 2;
+        }
+
+        foreach (int i in tamSayi)          // Foreach döngüsü ile ekrana yazdırıyoruz.
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.Write("Lütfen 11. elamanı girer misiniz?");   
+        int girilenDeger = Convert.ToInt32(Console.ReadLine());  // Kullanıcıdan 11. değeri alıyoruz
+
+        Array.Resize(ref tamSayi, tamSayi.Length+1);
+        tamSayi[tamSayi.Length - 1 ] = girilenDeger;
+
+        
+
+        Array.Sort(tamSayi);    // Sayılar dizisini küçükten büyüğe sıralıyoruz.
+        Array.Reverse(tamSayi);  // Küçükten büyüğe sıraladığımız sayıları ters çevirip büyükten küçüğe sıralıyoruz.
+
+        for (int i = 0; i< tamSayi.Length; i++)
+        {
+
+            Console.WriteLine(tamSayi[i]); 
+        }
+    }
+}
+  
